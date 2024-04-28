32f793a0ea9f2450dc9b1e41d6c58c3195092986
Soubhagyajit Borah <sjbtechcenter@gmail.com>
2024-04-28 16:06:02 +0530

    Importable Navbar and some eastereggs

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 
-document.addEventListener("DOMContentLoaded", function () {
+
+setTimeout(function () {
     const menuBtn = document.getElementById("menu-btn");
     const navLinks = document.getElementById("nav-links");
-    console.log("JavaScript loaded");
 
     // Add event listener to the menu button
     menuBtn.addEventListener("click", function () {
-        console.log("Menu button clicked"); // Log a message to the console
         // Toggle the visibility of the navigation links
         navLinks.classList.toggle("hidden");
     });
-
-
-});
+}, 500);
